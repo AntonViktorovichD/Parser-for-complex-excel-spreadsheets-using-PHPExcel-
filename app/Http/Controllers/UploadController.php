1d7b49c82b685c5d332b4665c16f5ae027bee230
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-03-17 14:40:36 +0300

    Empty table exception

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -54,152 +54,157 @@ class UploadController extends Controller {
         $mergeCells[] = $worksheet->getMergeCells();
         $highestRow = $worksheet->getHighestRow();
         $highestColumn = $worksheet->getHighestColumn();
+        $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
         $highestColumn++;
+            $counter = 1;
+            $arrKeys = [];
+            $arrVals = [];
+            $arrLett = [];
+            $arrCoord = [];
+            $colStart = [];
+            $colEnd = [];
+            $rowStart = [];
+            $rowEnd = [];
+            $arrStart = [];
+            $arrColEnd = [];
+            $arrRowEnd = [];
+            $arrEnd = [];
 
-        $counter = 1;
-        $arrKeys = [];
-        $arrVals = [];
-        $arrLett = [];
-        $arrCoord = [];
-        $colStart = [];
-        $colEnd = [];
-        $rowStart = [];
-        $rowEnd = [];
-        $arrStart = [];
-        $arrColEnd = [];
-        $arrRowEnd = [];
-        $arrEnd = [];
+            for ($column = 'A';
+                 $column != $highestColumn;
+                 $column++) {
+                $arrKeys[] = $column;
+                $arrVals[] = $counter++;
+            }
 
-        for ($column = 'A';
-             $column != $highestColumn;
-             $column++) {
-            $arrKeys[] = $column;
-            $arrVals[] = $counter++;
-        }
-
-        foreach ($mergeCells as $mergeCell) {
-            $arrLett = array_slice(preg_split('#\d+:|\d+#', implode(':', $mergeCell)), 0, count($arrLett) - 1);
-            foreach ($arrLett as $val) {
-                foreach (array_combine($arrKeys, $arrVals) as $key => $digit) {
-                    if ($val == $key) {
-                        $arrCoord[] = $digit;
+            foreach ($mergeCells as $mergeCell) {
+                $arrLett = array_slice(preg_split('#\d+:|\d+#', implode(':', $mergeCell)), 0, count($arrLett) - 1);
+                foreach ($arrLett as $val) {
+                    foreach (array_combine($arrKeys, $arrVals) as $key => $digit) {
+                        if ($val == $key) {
+                            $arrCoord[] = $digit;
+                        }
                     }
                 }
             }
-        }
 
-        $arrLett = array_slice(preg_split('#\D+:|\D+#', implode(':', $mergeCell)), 1);
+            $arrLett = array_slice(preg_split('#\D+:|\D+#', implode(':', $mergeCell)), 1);
 
-        foreach ($arrCoord as $key => $coord) {
-            if ($key == ($key % 2 == 0) || $key == 0) {
-                $colStart[] = $coord;
-            } else {
-                $colEnd[] = $coord;
-            }
-        }
-
-        foreach ($arrLett as $key => $coord) {
-            if ($key == ($key % 2 == 0) || $key == 0) {
-                $rowStart[] = $coord;
-            } else {
-                $rowEnd[] = $coord;
-            }
-        }
-
-        for ($i = 0; $i < count($colEnd); $i++) {
-            $arrColEnd[$i] = $colEnd[$i];
-        }
-
-        for ($i = 0; $i < count($rowEnd); $i++) {
-            $arrRowEnd[$i] = $rowEnd[$i];
-        }
-
-        for ($i = 0; $i < count($arrCoord); $i++) {
-            $arrCoord[$i] = $arrCoord[$i] . ':' . $arrLett[$i];
-        }
-
-        $arrCoord = array_chunk($arrCoord, 2);
-
-        for ($i = 0; $i < count($arrCoord); $i++) {
-            $arrStart[$i] = $arrCoord[$i][0];
-            $arrEnd[$i] = $arrCoord[$i][1];
-        }
-
-        $arrCoord = array_combine($arrEnd, $arrStart);
-
-        for ($row = 1; $row < $highestRow; $row++) {
-            $colCounter = 1;
-
-            $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
-
-            for ($col = 0; $col < $highestColumnIndex; $col++) {
-                $value = $worksheet->getCellByColumnAndRow($col, $row)->getValue();
-
-                $counter++;
-
-                if (isset($value)) {
-                    $colCounter = $col;
-                    $arr[$row][$col]['title'] = $value;
-                    $arr[$row][$col]['rowStart'] = $row;
-                    $arr[$row][$col]['rowEnd'] = $row;
-                    $arr[$row][$col]['colStart'] = $col + 1;
-                    $arr[$row][$col]['colEnd'] = $col + 1;
-                } elseif (isset($arr[$row][$col])) {
-                    $arr[$row][$colCounter]['colEnd'] = $col;
-                } elseif (empty($arr[1][0]['title'])) {
-                    $arr[$row][$col]['rowStart'] = $row;
-                    $arr[$row][$col]['colStart'] = $col + 1;
-                }
-
-                if (isset($arr[$row][$col]['title'])) {
-                    $arr[$row][$col]['id'] = $counter - $highestColumnIndex;
-                } elseif (empty($arr[1][0]['title'])) {
-                    $arr[$row][$col]['title'] = 'empty';
-                    $arr[$row][$col]['rowStart'] = $row;
-                    $arr[$row][$col]['colStart'] = $col + 1;
-                    $arr[$row][$col]['rowEnd'] = $row;
-                    $arr[$row][$col]['colEnd'] = $col + 1;
+            foreach ($arrCoord as $key => $coord) {
+                if ($key == ($key % 2 == 0) || $key == 0) {
+                    $colStart[] = $coord;
                 } else {
-                    unset($arr[$row][$col]);
+                    $colEnd[] = $coord;
                 }
             }
-        }
 
-        $arrCell = $arr;
-
-        for ($row = 1; $row < $highestRow; $row++) {
-
-            $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
-
-            for ($col = 0; $col < $highestColumnIndex; $col++) {
-
-                if (isset($arr[$row][$col]['title'])) {
-                    if ($arr[$row][$col]['title'] == 'empty') {
-                        $arrCell[1][0]['title'] = NULL;
-                    }
-                    $colStart = array_keys($arrCoord, $arr[$row][$col]['colStart'] . ':' . $arr[$row][$col]['rowStart']);
-                    foreach ($colStart as $cs) {
-                        $ce = explode(':', $cs);
-
-                        $arr[$row][$col]['colEnd'] = $ce[0];
-                        $arr[$row][$col]['rowEnd'] = $ce[1];
-                    }
-                    $arr[$row][$col]['colSpan'] = $arr[$row][$col]['colEnd'] - $arr[$row][$col]['colStart'] + 1;
-                    $arr[$row][$col]['rowSpan'] = $arr[$row][$col]['rowEnd'] - $arr[$row][$col]['rowStart'] + 1;
-                    $arrCell[$row][$col]['cell'] = '<td rowspan= ' . $arr[$row][$col]["rowSpan"] . ' colspan= ' . $arr[$row][$col]["colSpan"] . '>' . $arrCell[$row][$col]['title'] . '</td>';
+            foreach ($arrLett as $key => $coord) {
+                if ($key == ($key % 2 == 0) || $key == 0) {
+                    $rowStart[] = $coord;
                 } else {
-                    $arrCell[$row][$col] = NULL;
+                    $rowEnd[] = $coord;
                 }
             }
-        }
 
-        $date = date('Y:m:d H:i:s');
+            for ($i = 0; $i < count($colEnd); $i++) {
+                $arrColEnd[$i] = $colEnd[$i];
+            }
 
-        $json = json_encode($arrCell, JSON_UNESCAPED_UNICODE);
+            for ($i = 0; $i < count($rowEnd); $i++) {
+                $arrRowEnd[$i] = $rowEnd[$i];
+            }
 
-        DB::insert('insert into tables (json_val, table_name, created_at, highest_row, highest_column_index) values (?, ?, ?, ?, ?)', [$json, $filename, $date, $highestRow, $highestColumnIndex]);
+            for ($i = 0; $i < count($arrCoord); $i++) {
+                $arrCoord[$i] = $arrCoord[$i] . ':' . $arrLett[$i];
+            }
 
-        return redirect()->action([jsonController::class, 'arrayToJson']);
+            $arrCoord = array_chunk($arrCoord, 2);
+
+            for ($i = 0; $i < count($arrCoord); $i++) {
+                $arrStart[$i] = $arrCoord[$i][0];
+                $arrEnd[$i] = $arrCoord[$i][1];
+            }
+
+            $arrCoord = array_combine($arrEnd, $arrStart);
+
+        if (!empty($arrCoord)) {
+
+            for ($row = 1; $row < $highestRow; $row++) {
+                $colCounter = 1;
+
+                $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
+
+                for ($col = 0; $col < $highestColumnIndex; $col++) {
+                    $value = $worksheet->getCellByColumnAndRow($col, $row)->getValue();
+
+                    $counter++;
+
+                    if (isset($value)) {
+                        $colCounter = $col;
+                        $arr[$row][$col]['title'] = $value;
+                        $arr[$row][$col]['rowStart'] = $row;
+                        $arr[$row][$col]['rowEnd'] = $row;
+                        $arr[$row][$col]['colStart'] = $col + 1;
+                        $arr[$row][$col]['colEnd'] = $col + 1;
+                    } elseif (isset($arr[$row][$col])) {
+                        $arr[$row][$colCounter]['colEnd'] = $col;
+                    } elseif (empty($arr[1][0]['title'])) {
+                        $arr[$row][$col]['rowStart'] = $row;
+                        $arr[$row][$col]['colStart'] = $col + 1;
+                    }
+
+                    if (isset($arr[$row][$col]['title'])) {
+                        $arr[$row][$col]['id'] = $counter - $highestColumnIndex;
+                    } elseif (empty($arr[1][0]['title'])) {
+                        $arr[$row][$col]['title'] = 'empty';
+                        $arr[$row][$col]['rowStart'] = $row;
+                        $arr[$row][$col]['colStart'] = $col + 1;
+                        $arr[$row][$col]['rowEnd'] = $row;
+                        $arr[$row][$col]['colEnd'] = $col + 1;
+                    } else {
+                        unset($arr[$row][$col]);
+                    }
+                }
+            }
+
+            $arrCell = $arr;
+
+            for ($row = 1; $row < $highestRow; $row++) {
+
+                $highestColumnIndex = PHPExcel_Cell::columnIndexFromString($highestColumn);
+
+                for ($col = 0; $col < $highestColumnIndex; $col++) {
+
+                    if (isset($arr[$row][$col]['title'])) {
+                        if ($arr[$row][$col]['title'] == 'empty') {
+                            $arrCell[1][0]['title'] = NULL;
+                        }
+                        $colStart = array_keys($arrCoord, $arr[$row][$col]['colStart'] . ':' . $arr[$row][$col]['rowStart']);
+                        foreach ($colStart as $cs) {
+                            $ce = explode(':', $cs);
+
+                            $arr[$row][$col]['colEnd'] = $ce[0];
+                            $arr[$row][$col]['rowEnd'] = $ce[1];
+                        }
+                        $arr[$row][$col]['colSpan'] = $arr[$row][$col]['colEnd'] - $arr[$row][$col]['colStart'] + 1;
+                        $arr[$row][$col]['rowSpan'] = $arr[$row][$col]['rowEnd'] - $arr[$row][$col]['rowStart'] + 1;
+                        $arrCell[$row][$col]['cell'] = '<td rowspan= ' . $arr[$row][$col]["rowSpan"] . ' colspan= ' . $arr[$row][$col]["colSpan"] . '>' . $arrCell[$row][$col]['title'] . '</td>';
+                    } else {
+                        $arrCell[$row][$col] = NULL;
+                    }
+                }
+            }
+
+            $date = date('Y:m:d H:i:s');
+
+            $json = json_encode($arrCell, JSON_UNESCAPED_UNICODE);
+
+            DB::insert('insert into tables (json_val, table_name, created_at, highest_row, highest_column_index) values (?, ?, ?, ?, ?)', [$json, $filename, $date, $highestRow, $highestColumnIndex]);
+
+            return redirect()->action([jsonController::class, 'arrayToJson']);
 //        return view('arrayToJson', ['arr' => $json, 'tableload' => 'Таблица успешно загружена']);
+        } else {
+            return view('upload', ['ulerror' => 'Таблица пуста']);
+        }
     }
 }
