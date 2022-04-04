db27e3703f7417040b69d41b8fe7f41245ec2d39
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-04-04 12:02:21 +0300

    Fix DB connection

--- resources/views/arrayToJson.blade.php
+++ resources/views/arrayToJson.blade.php
@@ -9,18 +9,16 @@
 {{ $tableload }}
 
 <?php
+$row = 0;
 $arrs = json_decode($arr, true);
 $arr_rows = json_decode($arr_rows, true);
 echo '<ul>' . PHP_EOL;
-
 foreach ($arrs as $arr) {
     echo '<li><a href="/tables/' . $arr['table_name'] . '">' . $arr['table_name'] . '</a></li>';
-    if (count($arr_rows) > 0) {
-        foreach ($arr_rows as $row) {
-            if ($arr['table_uuid'] == $row['table_uuid']) {
-                echo '<a href="/edit/' . $arr['table_name'] . '"> Редактировать </a>';
-            }
-        }
+    foreach ($arr_rows as $row) {
+    }
+    if ($arr['table_uuid'] == $row['table_uuid']) {
+        echo '<a href="/edit/' . $arr['table_name'] . '"> Редактировать </a>';
     } else {
         echo '<a href="/add/' . $arr['table_name'] . '"> Добавить </a>';
     }
