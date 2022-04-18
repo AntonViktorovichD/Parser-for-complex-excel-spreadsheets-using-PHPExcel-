64497f83094ffd9575c19e94c9cfff29d2fea8c8
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-04-18 10:45:47 +0300

    Added independent components departs & districts

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -21,20 +21,16 @@
 </form>
 
 <?php
-
 $depart_helper = DB::table('depart_helper')->pluck('title');
 $depart_helper = (json_decode(json_encode($depart_helper, JSON_UNESCAPED_UNICODE), true));
 $depart_helper_id = DB::table('depart_helper')->pluck('id');
 $depart_helper_id = (json_decode(json_encode($depart_helper_id, JSON_UNESCAPED_UNICODE), true));
-
 echo '<h3>Типы Учреждений:</h3>';
-echo '<div id="v-model-multiple-checkboxes">';
-echo '<div  class="cols">';
-//echo '<div class="cols" id="v-model-multiple-checkboxes-depart">';
-foreach ($depart_helper as $counter => $depart) {
-    echo '<input type="checkbox" id=" ' . $depart . ' " v-model="checkedDeparts" data-checker="depart" @change="getStatus($event)" value=" ' . $depart_helper_id[$counter] . ' " data-value=" ' . $depart_helper_id[$counter] . ' "><label for="' . $depart . '">' . $depart . '</label><br />';
+echo '<div id="v-model-multiple-checkboxes checkboxes" >';
+echo '<div id="depart_checkboxes" class="cols">';
+foreach ($depart_helper as $depart_counter => $depart) {
+    echo '<input type="checkbox" id=" ' . $depart . ' " v-model="checkedDeparts" data-checker="depart"  @change="getStatus($event)" value=" ' . $depart_helper_id[$depart_counter] . ' " data-value=" ' . $depart_helper_id[$depart_counter] . ' "><label for="' . $depart . '">' . $depart . '</label><br />';
 }
-//echo '<span>Отмеченные имена: {{ checkedDeparts }}</span>';
 echo '{{ checkedDeparts }}';
 echo '</div>';
 $distr_helper = DB::table('distr_helper')->pluck('title');
@@ -42,8 +38,8 @@ $distr_helper = (json_decode(json_encode($distr_helper, JSON_UNESCAPED_UNICODE),
 $distr_helper_id = DB::table('distr_helper')->pluck('id');
 $distr_helper_id = (json_decode(json_encode($distr_helper_id, JSON_UNESCAPED_UNICODE), true));
 echo '<h3>Районы:</h3>';
-echo '<div  class="cols">';
-//echo '<div class="cols" id="v-model-multiple-checkboxes-distr">';
+echo '<div id="distr_checkboxes" class="cols">';
+
 foreach ($distr_helper as $counter => $distr) {
     echo '<input type="checkbox" id=" ' . $distr . '" v-model="checkedDistrs" data-checker="distr" @change="getStatus($event)" value=" ' . $distr_helper_id[$counter] . ' " data-value=" ' . $distr_helper_id[$counter] . ' "><label for="' . $distr . '">' . $distr . '</label><br />';
 }
@@ -59,7 +55,6 @@ $org_helper_id = DB::table('org_helper')->pluck('id');
 $org_helper_id = (json_decode(json_encode($org_helper_id, JSON_UNESCAPED_UNICODE), true));
 echo '<h3>Учреждения:</h3>';
 echo '<div  class="cols">';
-//echo '<div class="cols" id="v-model-multiple-checkboxes-org">';
 foreach ($org_helper as $counter => $org) {
     $org = preg_replace('#"#', '&quot', $org);
     echo '<input type="checkbox" class="org" id=" ' . $org . '" v-model="checkedOrg" @change="getStatus($event)" data-checker="org" data-departId=" ' . $org_depart_id[$counter] . ' " data-distrId=" ' . $org_distr_id[$counter] . ' " @change="getOrgStatus($event)" value=" ' . $org_helper_id[$counter] . ' "><label for="' . $org . '">' . $org . '</label><br />';
@@ -70,37 +65,52 @@ echo '</div>';
 
 <script src="/js/vue.global.js"></script>
 <script>
-    Vue.createApp({
+    const depart = Vue.createApp({
         data() {
             return {
                 checkedDeparts: [],
-                checkedDistrs: [],
-                checkedOrg: [],
             }
         },
         methods: {
-
             getStatus: function (e) {
-                if (e.target.dataset.checker == 'depart') {
-                    departOrg = document.querySelectorAll('.org');
-                    departOrg.forEach(function (depart) {
-                        if (depart.dataset.departid == e.target.dataset.value) {
-                            depart.checked = !depart.checked;
-                        }
-                    })
-                }
-                if (e.target.dataset.checker == 'distr') {
-                    distrOrg = document.querySelectorAll('.org');
-                    distrOrg.forEach(function (distr) {
-                        if (distr.dataset.departid == e.target.dataset.value) {
-                            distr.checked = !distr.checked;
-                        }
-                    })
+                for (let i = 0; i <= '<?php echo $depart_counter; ?>'; i++) {
+                    if (e.target.dataset.checker == 'depart') {
+                        departOrg = document.querySelectorAll('.org');
+                        departOrg.forEach(function (depart) {
+                            if (depart.dataset.departid == e.target.dataset.value) {
+                                depart.checked = e.target.checked;
+                            }
+                        })
+                    }
                 }
             }
-        }
-    }).mount('#v-model-multiple-checkboxes')
 
+        }
+    })
+    depart.mount('#depart_checkboxes')
+    const distr = Vue.createApp({
+        data() {
+            return {
+                checkedDistrs: [],
+            }
+        },
+        methods: {
+            getStatus: function (e) {
+                for (let i = 0; i <= '<?php echo $depart_counter; ?>'; i++) {
+                    if (e.target.dataset.checker == 'distr') {
+                        departOrg = document.querySelectorAll('.org');
+                        departOrg.forEach(function (distr) {
+                            if (distr.dataset.departid == e.target.dataset.value) {
+                                distr.checked = e.target.checked;
+                            }
+                        })
+                    }
+                }
+            }
+
+        }
+    })
+    distr.mount('#distr_checkboxes')
 </script>
 </body>
 </html>
