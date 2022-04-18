90b03eccca7eb76cb6b0197ab4f9dd311504dd41
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-04-18 15:25:13 +0300

    Added independent components orgs

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -28,7 +28,7 @@ echo '<h3>Типы Учреждений:</h3>';
 echo '<div id="v-model-multiple-checkboxes" >';
 echo '<div id="depart_checkboxes" class="cols">';
 foreach ($depart_helper as $depart_counter => $depart) {
-    echo '<input type="checkbox" class="depart" id=" ' . $depart . ' " v-model="checkedDeparts" data-checker="depart"  @change="getStatus($event)" value=" ' . $depart_helper_id[$depart_counter] . ' " data-value=" ' . $depart_helper_id[$depart_counter] . ' "><label for="' . $depart . '">' . $depart . '</label><br />';
+    echo '<input type="checkbox" class="depart" id=" ' . $depart . ' " v-model="checkedDeparts" data-checker="depart" @change="getStatus($event)" value=" ' . $depart_helper_id[$depart_counter] . ' " data-value=" ' . $depart_helper_id[$depart_counter] . ' "><label for="' . $depart . '">' . $depart . '</label><br />';
 }
 echo '</div>';
 $distr_helper = DB::table('distr_helper')->pluck('title');
@@ -37,11 +37,9 @@ $distr_helper_id = DB::table('distr_helper')->pluck('id');
 $distr_helper_id = (json_decode(json_encode($distr_helper_id, JSON_UNESCAPED_UNICODE), true));
 echo '<h3>Районы:</h3>';
 echo '<div id="distr_checkboxes" class="cols">';
-
 foreach ($distr_helper as $distr_counter => $distr) {
     echo '<input type="checkbox" class="distr" id=" ' . $distr . '" v-model="checkedDistrs" data-checker="distr" @change="getStatus($event)" value=" ' . $distr_helper_id[$distr_counter] . ' " data-value=" ' . $distr_helper_id[$distr_counter] . ' "><label for="' . $distr . '">' . $distr . '</label><br />';
 }
-//echo '<span>Отмеченные имена: {{ checkedDistrs }}</span>';
 echo '</div>';
 $org_helper = DB::table('org_helper')->pluck('title');
 $org_helper = (json_decode(json_encode($org_helper, JSON_UNESCAPED_UNICODE), true));
@@ -82,11 +80,9 @@ echo '</div>';
                     }
                 }
             }
-
         }
     })
     depart.mount('#depart_checkboxes')
-
     const distr = Vue.createApp({
         data() {
             return {
@@ -101,16 +97,15 @@ echo '</div>';
                         distrOrg.forEach(function (distr) {
                             if (distr.dataset.distrid == e.target.dataset.value) {
                                 distr.checked = e.target.checked;
+
                             }
                         })
                     }
                 }
             }
-
         }
     })
     distr.mount('#distr_checkboxes')
-
     const orgs = Vue.createApp({
         data() {
             return {
