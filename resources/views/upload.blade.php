d7f9a66f488339cb0dc9269062dae056b37a661e
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-04-20 13:04:35 +0300

    Checkbox with the ability to select by parameters

--- resources/views/upload.blade.php
+++ resources/views/upload.blade.php
@@ -18,50 +18,50 @@
     @csrf
     <input type="file" name="userfile">
     <input type="text" name="filename">
-@php
-    $depart_helper = DB::table('depart_helper')->pluck('title');
-    $depart_helper = (json_decode(json_encode($depart_helper, JSON_UNESCAPED_UNICODE), true));
-    $depart_helper_id = DB::table('depart_helper')->pluck('id');
-    $depart_helper_id = (json_decode(json_encode($depart_helper_id, JSON_UNESCAPED_UNICODE), true));
-    echo '<h3>Типы Учреждений:</h3>';
-    echo '<div id="v-model-multiple-checkboxes" >';
-    echo '<div id="checkboxes">';
-    echo '<div class="cols">';
-    foreach ($depart_helper as $depart_counter => $depart) {
-        echo '<input type="checkbox" class="depart" id=" ' . $depart . ' " v-model="checked" data-checker="depart" @change="getStatus($event)" value=" ' . $depart_helper_id[$depart_counter] . ' " data-value=" ' . $depart_helper_id[$depart_counter] . ' "><label for="' . $depart . '">' . $depart . '</label><br />';
-    }
-    echo '</div>';
-    $distr_helper = DB::table('distr_helper')->pluck('title');
-    $distr_helper = (json_decode(json_encode($distr_helper, JSON_UNESCAPED_UNICODE), true));
-    $distr_helper_id = DB::table('distr_helper')->pluck('id');
-    $distr_helper_id = (json_decode(json_encode($distr_helper_id, JSON_UNESCAPED_UNICODE), true));
-    echo '<h3>Районы:</h3>';
-    echo '<div class="cols">';
-    foreach ($distr_helper as $distr_counter => $distr) {
-        echo '<input type="checkbox" class="distr" id=" ' . $distr . '" v-model="checked" data-checker="distr" @change="getStatus($event)" value=" ' . $distr_helper_id[$distr_counter] . ' " data-value=" ' . $distr_helper_id[$distr_counter] . ' "><label for="' . $distr . '">' . $distr . '</label><br />';
-    }
-    echo '</div>';
+    @php
+        $depart_helper = DB::table('depart_helper')->pluck('title');
+        $depart_helper = (json_decode(json_encode($depart_helper, JSON_UNESCAPED_UNICODE), true));
+        $depart_helper_id = DB::table('depart_helper')->pluck('id');
+        $depart_helper_id = (json_decode(json_encode($depart_helper_id, JSON_UNESCAPED_UNICODE), true));
+        echo '<h3>Типы Учреждений:</h3>';
+        echo '<div id="v-model-multiple-checkboxes" >';
+        echo '<div id="checkboxes">';
+        echo '<div class="cols">';
+        foreach ($depart_helper as $depart_counter => $depart) {
+            echo '<input type="checkbox" class="depart" id=" ' . $depart . ' " v-model="checked" data-checker="depart" @change="getStatus($event)" value=" ' . $depart_helper_id[$depart_counter] . ' " data-value=" ' . $depart_helper_id[$depart_counter] . ' "><label for="' . $depart . '">' . $depart . '</label><br />';
+        }
+        echo '</div>';
+        $distr_helper = DB::table('distr_helper')->pluck('title');
+        $distr_helper = (json_decode(json_encode($distr_helper, JSON_UNESCAPED_UNICODE), true));
+        $distr_helper_id = DB::table('distr_helper')->pluck('id');
+        $distr_helper_id = (json_decode(json_encode($distr_helper_id, JSON_UNESCAPED_UNICODE), true));
+        echo '<h3>Районы:</h3>';
+        echo '<div class="cols">';
+        foreach ($distr_helper as $distr_counter => $distr) {
+            echo '<input type="checkbox" class="distr" id=" ' . $distr . '" v-model="checked" data-checker="distr" @change="getStatus($event)" value=" ' . $distr_helper_id[$distr_counter] . ' " data-value=" ' . $distr_helper_id[$distr_counter] . ' "><label for="' . $distr . '">' . $distr . '</label><br />';
+        }
+        echo '</div>';
 
-    echo '</div>';
-    $org_helper = DB::table('org_helper')->pluck('title');
-    $org_helper = (json_decode(json_encode($org_helper, JSON_UNESCAPED_UNICODE), true));
-    $org_depart_id = DB::table('org_helper')->pluck('depart_id');
-    $org_depart_id = (json_decode(json_encode($org_depart_id, JSON_UNESCAPED_UNICODE), true));
-    $org_distr_id = DB::table('org_helper')->pluck('distr_id');
-    $org_distr_id = (json_decode(json_encode($org_distr_id, JSON_UNESCAPED_UNICODE), true));
-    $org_helper_id = DB::table('org_helper')->pluck('id');
-    $org_helper_id = (json_decode(json_encode($org_helper_id, JSON_UNESCAPED_UNICODE), true));
-    echo '<h3>Учреждения:</h3>';
-    echo '<div id="org_checkboxes" class="cols">';
-    foreach ($org_helper as $org_counter => $org) {
-        $org = preg_replace('#"#', '&quot', $org);
-        echo '<input type="checkbox" class="org" id=" ' . $org . '" v-model="checkedOrg" @change="getStatus($event)" data-checker="org" data-departId=" ' . $org_depart_id[$org_counter] . ' " data-distrId=" ' . $org_distr_id[$org_counter] . ' " @change="getOrgStatus($event)" value=" ' . $org_helper_id[$org_counter] . ' "><label for="' . $org . '">' . $org . '</label><br />';
-    }
-    echo '</div>';
-@endphp
-<script src="/js/vue.global.js"></script>
-<script src="/js/vue.upload.js"></script>
+        echo '</div>';
+        $org_helper = DB::table('org_helper')->pluck('title');
+        $org_helper = (json_decode(json_encode($org_helper, JSON_UNESCAPED_UNICODE), true));
+        $org_depart_id = DB::table('org_helper')->pluck('depart_id');
+        $org_depart_id = (json_decode(json_encode($org_depart_id, JSON_UNESCAPED_UNICODE), true));
+        $org_distr_id = DB::table('org_helper')->pluck('distr_id');
+        $org_distr_id = (json_decode(json_encode($org_distr_id, JSON_UNESCAPED_UNICODE), true));
+        $org_helper_id = DB::table('org_helper')->pluck('id');
+        $org_helper_id = (json_decode(json_encode($org_helper_id, JSON_UNESCAPED_UNICODE), true));
+        echo '<h3>Учреждения:</h3>';
+        echo '<div id="org_checkboxes" class="cols">';
+        foreach ($org_helper as $org_counter => $org) {
+            $org = preg_replace('#"#', '&quot', $org);
+            echo '<input type="checkbox" class="org" id=" ' . $org . '" v-model="checkedOrg" @change="getStatus($event)" data-checker="org" data-departId=" ' . $org_depart_id[$org_counter] . ' " data-distrId=" ' . $org_distr_id[$org_counter] . ' " @change="getOrgStatus($event)" value=" ' . $org_helper_id[$org_counter] . ' "><label for="' . $org . '">' . $org . '</label><br />';
+        }
+        echo '</div>';
+    @endphp
     <input type="submit">
 </form>
+<script src="/js/vue.global.js"></script>
+<script src="/js/vue.upload.js"></script>
 </body>
 </html>
