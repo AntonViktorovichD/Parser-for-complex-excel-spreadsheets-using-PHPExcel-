2bd2a8620556774ae075376f26c2bff8f71bde53
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-04-28 13:12:05 +0300

    Fixing menu`s and some front issues

--- resources/views/arrayToJson.blade.php
+++ resources/views/arrayToJson.blade.php
@@ -3,12 +3,35 @@
 <head>
     <meta charset="UTF-8">
     <title>Table</title>
+    <style>
+        .envelope {
+            margin-left: 30px;
+            margin-top: 20px;
+        }
+        a, a:hover, a:active {
+            text-decoration: none;
+            font-size: 17px;
+            color: black;
+            margin-bottom: 10px;
+        }
+
+        li {
+            list-style-type: none;
+        }
+
+        ul {
+            margin-left: 0;
+            padding-left: 0;
+        }
+    </style>
 </head>
 <body>
 
 {{ $tableload }}
 
 @php
+echo '<div class="envelope">';
+echo '<a href="/">На главную</a>';
     $row = 0;
     $arrs = json_decode($arr, true);
     $arr_rows = json_decode($arr_rows, true);
@@ -32,6 +55,7 @@
     }
     }
     echo '</ul>' . PHP_EOL;
+    echo '</div>' . PHP_EOL;
 @endphp
 </body>
 </html>
