66ec2c268637c7c61c6351a365172f107e1d6fd0
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-05-23 13:21:33 +0300

    Sum function

--- resources/views/ultest.blade.php
+++ resources/views/ultest.blade.php
@@ -10,32 +10,46 @@
     th, td {
         border: 1px solid black;
         padding: 5px;
-        min-width: 30px;
+        height: 30px;
     }
 </style>
 
 @php
     echo '<table>' . PHP_EOL;
-        echo '<tr>' . PHP_EOL;
-for($i = 0; $i < 27; $i++) {
-if (isset($sum[$i])) {
-echo '<td><span class="'. $i . '">' . $sum[$i] . '</span></td>' . PHP_EOL;
-} else {
-echo '<td></td>' . PHP_EOL;
+    echo '<tr>' . PHP_EOL;
+var_dump($sum);
+for ($i = 1; $i <= $highestColumnIndex; $i++) {
+   echo '<td id="'. $i. '" class="visible_cell"></td>' . PHP_EOL;
 }
-}
-        echo '</tr>' . PHP_EOL;
+    for ($i = 1; $i <= $highestColumnIndex; $i++) {
+        if (isset($sum[$i])) {
+            echo '<td hidden><span class="sum_cell" data-target="'. $i. '">' . $sum[$i] . '</span></td>' . PHP_EOL;
+        } else {
+            echo '<td hidden></td>' . PHP_EOL;
+        }
+    }
+    echo '</tr>' . PHP_EOL;
     echo '</table>' . PHP_EOL;
 @endphp
 @include('layouts.footer')
 
 <script>
     window.onload = () => {
-        let sum_target_cells = document.querySelectorAll('span');
-        let tds = document.querySelectorAll('td');
-        for(let i = 0; i < sum_target_cells.length; i++){
-            for(let k = 0; k < sum_target_cells[i].innerHTML.split(',').length; k++){
-                console.log(sum_target_cells[i].innerHTML.split(',')[k]);
+        let sum_target_cells = document.querySelectorAll('.sum_cell');
+        let tds = document.querySelectorAll('.visible_cell');
+        // console.log(tds);
+        for (let i = 0; i < sum_target_cells.length; i++) {
+            for (let k = 0; k < sum_target_cells[i].innerHTML.split(',').length; k++) {
+                if (sum_target_cells[i].innerHTML.includes(':')) {
+                    sum_target_cells[i].innerHTML;
+                } else {
+                    let span = document.createElement('span');
+                    let parent = tds[sum_target_cells[i].dataset.target - 1];
+                    span.innerHTML = 1111;
+                    parent.appendChild(span);
+                    // console.log(sum_target_cells[i].dataset.target);
+                    break;
+                }
             }
         }
     }
