977eb9680301c40b284a942083477570ee0ac525
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-06-16 11:07:11 +0300

    SendUsersController filter

--- app/Http/Controllers/SendUsersController.php
+++ app/Http/Controllers/SendUsersController.php
@@ -11,13 +11,20 @@ use Illuminate\Support\Facades\Auth;
 class SendUsersController extends Controller {
     public function send() {
         $users = json_decode(DB::table('users')->orderBy('district', 'asc')->orderBy('department', 'asc')->get(), true);
+        $districts = DB::table('distr_helper')->orderBy('id', 'asc')->get();
+        $departments = DB::table('depart_helper')->orderBy('id', 'asc')->get();
         for ($i = 0; $i < count($users); $i++) {
             $users[$i]["org"] = DB::table('org_helper')->where('depart_id', '=', $users[$i]["department"])->where('distr_id', '=', $users[$i]["district"])->value('title');
+            $users[$i]["district_id"] = $users[$i]["district"];
             $users[$i]["district"] = DB::table('distr_helper')->where('id', '=', $users[$i]["district"])->value('title');
+            $users[$i]["department_id"] = $users[$i]["department"];
             $users[$i]["department"] = DB::table('depart_helper')->where('id', '=', $users[$i]["department"])->value('title');
             unset($users[$i]["remember_token"], $users[$i]["password"], $users[$i]["created_at"], $users[$i]["updated_at"]);
         }
+//        echo '<pre>';
+//        var_dump($users);
+//        echo '</pre>';
         $users = json_encode($users, JSON_UNESCAPED_UNICODE);
-        return view('send_users', compact('users'));
+        return view('send_users', compact('users', 'departments', 'districts'));
     }
 }
--- resources/views/send_users.blade.php
+++ resources/views/send_users.blade.php
@@ -1,5 +1,5 @@
 @include('layouts.header')
-@include('layouts.menu')
+{{--@include('layouts.menu')--}}
 <meta charset="UTF-8">
 <style>
     table {
@@ -17,11 +17,29 @@
         padding: 5px;
         min-width: 30px;
     }
+
+    .display {
+        display: none;
+    }
 </style>
 <table>
     <tr>
-        <th>Район</th>
-        <th>Тип</th>
+        <th>
+            <select id="district" class="form-select distr" aria-label="Default select example">
+                <option selected>Район</option>
+                @foreach($districts as $key => $district)
+                    <option value="{{ $key }}"> {{ $district->title }}</option>
+                @endforeach
+            </select>
+        </th>
+        <th>
+            <select id="department" class="form-select" aria-label="Default select example">
+                <option selected>Тип</option>
+                @foreach($departments as $key => $department)
+                    <option value="{{ $key }}"> {{ $department->title }}</option>
+                @endforeach
+            </select>
+        </th>
         <th>Учреждение</th>
         <th>E-mail</th>
         <th>Sms</th>
@@ -31,7 +49,7 @@
     @php
         $users = json_decode($users, true);
         foreach ($users as $user) {
-            echo '<tr>';
+            echo '<tr class="user" id="' . $user['id'] . '" data-district="' . $user['district_id'] . '" data-department="' . $user['department_id'] . '">';
             echo '<td>' . $user['district'] . '</td>';
             echo '<td>' . $user['department'] . '</td>';
             echo '<td>' . $user['org'] . '</td>';
@@ -42,5 +60,53 @@
             echo '</tr>';
         }
     @endphp
+
 </table>
 @include('layouts.footer')
+<script>
+    window.onload = () => {
+        let users = document.querySelectorAll('.user');
+        let district = 0;
+        let department = 0;
+        let distr = [];
+        let distr_arr = [];
+        let user_arr = Array.from(users);
+        let depart = [];
+        let depart_arr = [];
+        document.addEventListener('input', e => {
+                if (e.target.id == 'district') {
+                    district = e.target.selectedIndex;
+                }
+                if (e.target.id == 'department') {
+                    department = e.target.selectedIndex;
+                }
+                distr.length = 0;
+                depart.length = 0;
+
+                for (let i = 0; i < users.length; i++) {
+                    users[i].hidden = false;
+                    if (users[i].dataset.district == district) {
+                        distr.push(users[i]);
+                    }
+                    if (users[i].dataset.department == department) {
+                        depart.push(users[i])
+                    }
+                }
+
+                if (distr.length == 0) {
+                    depart_arr = user_arr.filter(num => !depart.includes(num)).concat(depart.filter(num => !user_arr.includes(num)));
+                    for (let i = 0; i < depart_arr.length; i++) {
+                        depart_arr[i].hidden = true;
+                    }
+                }
+
+                if (depart.length == 0) {
+                    distr_arr = user_arr.filter(num => !distr.includes(num)).concat(distr.filter(num => !user_arr.includes(num)));
+                    for (let i = 0; i < distr_arr.length; i++) {
+                        distr_arr[i].hidden = true;
+                    }
+                }
+            }
+        )
+    }
+</script>
