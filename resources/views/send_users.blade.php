2ebb0aed5df4db81fac128de2f424d6d5c032e63
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-06-17 09:30:16 +0300

    SendUsersController filter need fix

--- resources/views/send_users.blade.php
+++ resources/views/send_users.blade.php
@@ -71,6 +71,7 @@
         let distr_depart_arr = [];
         let users_arr = [];
         let difference = [];
+        let intersection = [];
         document.addEventListener('input', e => {
                 if (e.target.id == 'district') {
                     district = e.target.selectedIndex;
@@ -79,23 +80,31 @@
                     department = e.target.selectedIndex;
                 }
 
+
                 users_arr.length = 0;
                 difference.length = 0;
                 distr_depart_arr = [district, department];
 
+                console.log(distr_depart_arr);
+
                 for (let i = 0; i < users.length; i++) {
                     if (users[i].dataset.department == distr_depart_arr[1] && users[i].dataset.district == distr_depart_arr[0]) {
                         users_arr.push(users[i]);
-                    } else if (users[i].dataset.department == distr_depart_arr[1] && distr_depart_arr[0] == 0) {
+                    }
+                    if (users[i].dataset.department == distr_depart_arr[1] && distr_depart_arr[0] == 0) {
                         users_arr.push(users[i]);
-                    } else if (users[i].dataset.district == distr_depart_arr[0] && distr_depart_arr[1] == 0) {
+                    }
+                    if (users[i].dataset.district == distr_depart_arr[0] && distr_depart_arr[1] == 0) {
                         users_arr.push(users[i]);
-                    } else if (district == 0 && department == 0) {
+                    }
+                    if (district == 0 && department == 0) {
                         users_arr;
                     }
                 }
+
                 if (users_arr.length > 0) {
                     difference = users.filter(num => !users_arr.includes(num)).concat(users_arr.filter(num => !users.includes(num)));
+                    // intersection = users.filter(num => users_arr.includes(num));
                     for (let k = 0; k < difference.length; k++) {
                         difference[k].hidden = true;
                     }
