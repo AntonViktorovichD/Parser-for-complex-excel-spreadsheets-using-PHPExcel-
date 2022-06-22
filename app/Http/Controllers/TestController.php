8c4d95aa438a5fd6ab7c81d1001e5d314b2ffd0a
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-06-22 09:56:00 +0300

    Finish mail_sender on mailtrap.io

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -3,22 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
-use PHPExcel_Cell;
-use PHPExcel_IOFactory;
 use App\Http\Requests;
 use Illuminate\Support\Facades\DB;
 
 class TestController extends Controller {
     public function test(Request $request) {
-        $table_uuid = $request->get('table_uuid');
-        $table = DB::table('tables')->where('table_uuid', '=', $table_uuid)->get();
-        $departments = json_decode($table[0]->departments, true);
-        var_dump($departments);
+        $notification_rights = DB::table('notification_rights')->where('id', '=', 1)->get()[0];
+        $notifications = [];
+        if ($notification_rights->e_mail != 0 || $notification_rights->mobile_phone != 0) {
+            $table_uuid = $request->get('table_uuid');
+            $table = DB::table('tables')->where('table_uuid', '=', $table_uuid)->get();
+            $table_name = $table[0]->table_name;
+            $departments = json_decode($table[0]->departments, true);
+            foreach ($departments as $department) {
+                $notification = DB::table('user_noifications')->where('org_id', '=', $department)->get()[0];
+                $notifications[$department]['e_mail'] = $notification->e_mail;
+                $notifications[$department]['specialist_mobile_phone'] = $notification->specialist_mobile_phone;
+                $notifications[$department]['directors_mobile_phone'] = $notification->directors_mobile_phone;
+            }
 //        foreach ($table as $key => $tab) {
 //            echo '<pre>';
 //            var_dump($tab);
 //            echo '</pre>';
 //        }
+        }
+        foreach ($notifications as $key => $notification) {
+            if($notification['e_mail'] == 1) {
+                var_dump(DB::table('users')->where('department', '=', $key)->value('email'));
+            }
+        }
     }
 }
