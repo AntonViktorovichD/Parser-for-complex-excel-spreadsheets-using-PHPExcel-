2dd5953618aaf45e8cd0edeaa945807d9b4394f4
AntonViktorovichD <white.trash.without.cash@gmail.com>
2022-08-01 15:56:09 +0300

    remote host

--- resources/views/phones.blade.php
+++ resources/views/phones.blade.php
@@ -1,11 +1,36 @@
 @include('layouts.header')
 @include('layouts.menu')
-<div class="container">
-    @php
-    foreach($users as $user) {
-       var_dump($user[0]);
+<style>
+    .container-flex {
+        margin-left: 40px;
     }
-    @endphp
+
+    table {
+        width: auto !important;
+    }
+</style>
+<div class="container-flex">
+    <h1>Справочник телефонов специалистов</h1>
+    <table class="table table-striped table-borderless ">
+        <thead>
+        <tr>
+            <th scope="col">ФИО ответственного специалиста</th>
+            <th scope="col">Контактный телефон</th>
+        </tr>
+        </thead>
+        <tbody>
+
+            @php
+                foreach($users as $user) {
+   echo '<tr>';
+                        echo '<td>' . $user[0]["name"] .'</td>';
+                     echo '<td>' . $user[0]["city_phone"] .'</td>';
+             echo '</tr>';
+}
+            @endphp
+
+        </tbody>
+    </table>
 </div>
 
 {{ $elev_users->links() }}
